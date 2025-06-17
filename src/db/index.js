d2fc0e70c0c25b144d58ec68735ecb76c4237c59
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-06-17 18:15:20 +0530

    Remove not required code

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log(`\n MongoDB connected!...! DB host: ${conn.connection.host}`);
   } catch (error) {
     console.log(`MongoDB connection error`, error);
