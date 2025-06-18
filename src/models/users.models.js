90e0baaad878403338b0e97cf169cef1eb1e1da6
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-06-18 12:54:55 +0530

    Make user model

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,15 +1,49 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary...
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary...
+    },
+    watchHistory: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Video",
+      },
+    ],
+
+    password: {
+      type: String,
+      required: [true, "Password is required!..."],
+    },
+    refreshToken: {
+      type: String,
+      required: [true, "Refresh token is required!..."],
+    },
   },
-  email: String,
-});
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
