66fc036f8b319dd740ea8406f4c6a13f2f286622
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-06-18 16:49:03 +0530

    add new user-controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,1 +1,8 @@
+import { asyncErrorHandler } from "../utils/asyncErrorHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
+const registerUser = asyncErrorHandler(async (req, res, next) => {
+  res.status(200).json({
+    response: new ApiResponse(200, "All working"),
+  });
+});
