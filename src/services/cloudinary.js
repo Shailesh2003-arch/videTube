4d995c40417da1cefe27a0390f771718af6de008
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-06-18 15:28:31 +0530

    Add uploadFile method in cloudinary service

--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -6,3 +6,20 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary = async (localFilPath) => {
+  try {
+    if (!localFilPath) return null;
+    //  upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilPath, {
+      resource_type: "auto",
+    });
+    console.log(`File is uploaded successfully on cloudinary`, response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilPath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
