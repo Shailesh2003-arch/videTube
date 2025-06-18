70f1dfc36624eafa432bd23a7d25cbd89e781fba
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-06-18 14:07:29 +0530

    add method on user schema

---
+++ src/models/videos.models.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary
+      required: true,
+    },
+    thumbnail: {
+      type: String, // cloudinary
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // time from cloudinary
+      required: true,
+    },
+    views: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    videoOwner: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
