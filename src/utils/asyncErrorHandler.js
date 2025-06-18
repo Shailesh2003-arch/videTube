f155b368ec0112ec7c6fc51189ce826c6604d3ef
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-06-18 16:02:39 +0530

    comment some code in asyncErrorHandler

--- src/utils/asyncErrorHandler.js
+++ src/utils/asyncErrorHandler.js
@@ -6,12 +6,12 @@ const asyncErrorHandler = (requestHandler) => {
 
 export { asyncErrorHandler };
 
-const aysncHandler = (fn) => {
-  return async (req, res, next) => {
-    try {
-      await fn(req, res, next);
-    } catch (error) {
-      console.log(`Error:`, error);
-    }
-  };
-};
+// const aysncHandler = (fn) => {
+//   return async (req, res, next) => {
+//     try {
+//       await fn(req, res, next);
+//     } catch (error) {
+//       console.log(`Error:`, error);
+//     }
+//   };
+// };
