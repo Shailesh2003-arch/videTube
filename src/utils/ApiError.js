158c67882a3bc9a8063691c91667ba99dfc3bf73
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-06-19 14:13:29 +0530

    rectify error handling code

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -8,8 +8,7 @@ class ApiError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.data = null;
-    this.message = message;
-    this.sucess = "fail";
+    this.success = false;
     this.errors = errors;
 
     if (stack) {
