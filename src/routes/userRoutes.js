e6e0aaf151dcffc0b9253ec2d3e2fe5d9dd20e0a
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-06-24 15:20:46 +0530

    add update-profile route to the codebase

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,7 @@ import {
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateExistingDetailsOfUser,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -21,5 +22,6 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/update-profile").patch(verifyJWT, updateExistingDetailsOfUser);
 
 export default router;
