443e90b0e54c56c764e2b309de7b3f481c067de6
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-06-24 15:05:22 +0530

    add updateUserSettings to the codebase

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -240,4 +240,58 @@ const refreshAccessToken = asyncErrorHandler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+// oldPassword change functionality
+
+const changeCurrentPassword = asyncErrorHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.user?._id);
+  console.log(user);
+  console.log(user.password);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Invalid Password");
+  }
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+  res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
+// get current user...
+
+const getCurrentUser = asyncErrorHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "User fetched Successfully"));
+});
+
+// Updating other details of the user...
+
+const updateExistingDetailsOfUser = asyncErrorHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+  if (!fullName || !email) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: { fullName, email },
+    },
+    { new: true }
+  ).select("-password");
+  res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"));
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  getCurrentUser,
+  changeCurrentPassword,
+  updateExistingDetailsOfUser,
+};
