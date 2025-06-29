ab2920aec4fdcda9b721a054d261afe84f1ca36a
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-06-29 13:53:10 +0530

    add mongoose.Schema.Types.ObjectId

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -31,7 +31,7 @@ const userSchema = new Schema(
     },
     watchHistory: [
       {
-        type: mongoose.Schema.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: "Video",
       },
     ],
