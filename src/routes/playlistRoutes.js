e90151124042e436cac279a96472828be901cd40
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-07-09 17:47:52 +0530

    add playlist routes (add,remove video from a playlist)

--- src/routes/playlistRoutes.js
+++ src/routes/playlistRoutes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import {
+  createPlaylist,
+  addVideoToPlaylist,
+  removeVideoFromAPlaylist,
+} from "../controllers/playlist.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/").post(createPlaylist);
+router.route("/add/:playlistId/:videoId").patch(addVideoToPlaylist);
+router.route("/remove/:playlistId/:videoId").patch(removeVideoFromAPlaylist);
 
 export default router;
