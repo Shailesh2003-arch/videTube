e56eb3b5621434ff94d725f56c820317dca09738
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-07-09 13:25:44 +0530

    add createPlaylist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,23 +1,38 @@
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
-import Video from "../models/videos.models.js";
-import User from "../models/users.models.js";
+import { Playlist } from "../models/playlist.models.js";
 import { asyncErrorHandler } from "../utils/asyncErrorHandler.js";
 
 // create a playlist...
 
 const createPlaylist = asyncErrorHandler(async (req, res) => {
-  const { name, description } = req.body;
+  const name = req.body.name?.trim();
+  const description = req.body.description?.trim();
+
+  const owner = req.user._id;
+  if (!name) {
+    throw new ApiError(400, "Name of the playlist is required");
+  }
+  if (!description) {
+    throw new ApiError(400, "Description of the playlist is required");
+  }
+  const newPlaylist = await Playlist.create({
+    name,
+    description,
+    owner,
+  });
+
+  const playListObject = newPlaylist.toObject();
+  delete playListObject._id;
+  delete playListObject.updatedAt;
+
   console.log(`Name of the playlist : ${name}`);
-  console.log(`description of the playlist : ${description}`);
+  console.log(`Description of the playlist : ${description}`);
+  console.log(`Owner of the playlist : ${owner}`);
   res
-    .status(200)
+    .status(201)
     .json(
-      new ApiResponse(
-        200,
-        { name, description },
-        "Playlist created successfully ✅"
-      )
+      new ApiResponse(200, playListObject, "Playlist created successfully ✅")
     );
 });
 
