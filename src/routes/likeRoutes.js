39598e9d9f71b841e07729ded5b70b569ea48d93
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-07-12 10:48:57 +0530

    add like routes in routes dir

---
+++ src/routes/likeRoutes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  toggleVideoLike,
+  getLikedVideos,
+  toggleCommentLike,
+} from "../controllers/like.controllers.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/toggle/v/:videoId").post(toggleVideoLike);
+router.route("/videos").get(getLikedVideos);
+router.route("/toggle/c/:commentId").post(toggleCommentLike);
+
+export default router;
