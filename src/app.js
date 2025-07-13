5b72eaa7c988c66dbc51df12d8546c23f8fd5dc6
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-07-13 20:50:13 +0530

    Fixed routing with versioning

--- src/app.js
+++ src/app.js
@@ -12,13 +12,9 @@ app.use(
 
 // for parsing incoming request body
 app.use(express.json({ limit: "16kb" }));
-
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
 // for serving static files...
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
 
 // [IMPORTS]
