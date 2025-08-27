ca88a8899c258068dc14e414b396548c7e42f03f
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-08-27 22:03:59 +0530

    add notification model to the codebase

---
+++ src/models/notification.models.js
@@ -0,0 +1,29 @@
+import mongoose, { mongo, Schema } from "mongoose";
+
+const notificationSchema = new Schema(
+  {
+    reciepent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["videoLike", "videoComment", "postLike", "postComment"],
+      required: true,
+    },
+    video: { type: mongoose.Schema.Types.ObjectId, ref: "Video" },
+    comment: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
+    tweet: { type: mongoose.Schema.Types.ObjectId, ref: "Tweet" },
+    message: { type: String },
+    isRead: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export const Notification = mongoose.model("Notification", notificationSchema);
