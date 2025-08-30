ba860d544aeb953a8a535c91ebe82e3c679da472
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-08-30 16:38:14 +0530

    add some commented code for websocket

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./src/.env" });
 import connectDB from "./db/index.js";
 
-import { app } from "./app.js";
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 4000;
-
+import { app /* httpServer*/ } from "./app.js";
 connectDB()
   .then(() => {
     app.listen(process.env.PORT, hostname, () => {
