7c8d7fdc723a58964f836eeb436e3ecbb1139a93
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-06 22:43:22 +0530

    add modified tweets route

--- src/routes/tweetRoutes.js
+++ src/routes/tweetRoutes.js
@@ -1,17 +1,20 @@
 import Router from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.js";
 import {
   createTweet,
   deleteTweet,
   updateTweet,
   getUserTweets,
+  getAllTweets,
 } from "../controllers/tweet.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("/").post(createTweet);
+router.route("/create").post(upload.single("image"), createTweet);
+router.route("/tweets").get(getAllTweets);
 router.route("/:userId").get(getUserTweets);
 router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 
