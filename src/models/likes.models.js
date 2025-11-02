03730c51419eba2f58d436130bbee5bdb98bccd0
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-11-02 20:33:20 +0530

    add enum types

--- src/models/likes.models.js
+++ src/models/likes.models.js
@@ -18,8 +18,28 @@ const likesSchema = new Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+    type: {
+      type: String,
+      enum: ["like", "dislike"],
+      default: "like",
+    },
   },
   { timestamps: true }
+);
+
+
+// ensure one reaction per user per target
+likesSchema.index(
+  { likedBy: 1, video: 1 },
+  { unique: true, partialFilterExpression: { video: { $exists: true } } }
+);
+likesSchema.index(
+  { likedBy: 1, comment: 1 },
+  { unique: true, partialFilterExpression: { comment: { $exists: true } } }
+);
+likesSchema.index(
+  { likedBy: 1, tweet: 1 },
+  { unique: true, partialFilterExpression: { tweet: { $exists: true } } }
 );
 
 export const Like = mongoose.model("Like", likesSchema);
