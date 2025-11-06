6f459a091df4298868a69667f03750c7592d7c17
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-11-06 17:31:49 +0530

    add comment routes

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -4,13 +4,15 @@ import {
   addComment,
   deleteComment,
   updateComment,
-  getVideoComments
+  getVideoComments, likeComment, dislikeComment
 } from "../controllers/comment.controllers.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/:videoId").get(getVideoComments).post(addComment);
-router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
+router.route("/comment/:commentId").delete(deleteComment).patch(updateComment);
+router.route("/:commentId/like").post(likeComment)
+router.route("/:commentId/dislike").post(dislikeComment)
 
 export default router;
