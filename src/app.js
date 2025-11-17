4a0122f554dde6518a17096522491e0530f20f81
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-11-17 20:48:12 +0530

    add app

--- src/app.js
+++ src/app.js
@@ -20,25 +20,27 @@ app.use(cookieParser());
 
 // imports
 import userRouter from "./routes/userRoutes.js";
+import authRouter from "./routes/authRoutes.js";
 import videoRouter from "./routes/videoRoutes.js";
 import subscriptionRouter from "./routes/subscriptionRoutes.js";
 import playlistRouter from "./routes/playlistRoutes.js";
 import likeRouter from "./routes/likeRoutes.js";
 import commentRouter from "./routes/commentRoutes.js";
 import tweetRouter from "./routes/tweetRoutes.js";
-import notificationRouter from "./routes/notificationRoutes.js"
-import commentReplyRouter from "./routes/commentReplyRoutes.js"
+import notificationRouter from "./routes/notificationRoutes.js";
+import commentReplyRouter from "./routes/commentReplyRoutes.js";
 
 // routes
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/users/notifications",notificationRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users/notifications", notificationRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/users/subscription", subscriptionRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comments", commentRouter);
-app.use("/api/v1/reply",commentReplyRouter)
+app.use("/api/v1/reply", commentReplyRouter);
 app.use("/api/v1/users/tweets", tweetRouter);
-app.use("/api/v1/users/playlist", playlistRouter);
+app.use("/api/v1/playlists", playlistRouter);
 
 // error handler
 app.use((err, req, res, next) => {
