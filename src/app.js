965f740c8772aa15a3814e5cb73bd481e561f5c7
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-11-19 12:36:48 +0530

    add FRONTEND LIVE URLs

--- src/app.js
+++ src/app.js
@@ -8,10 +8,7 @@ const app = express();
 // middlewares
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://vid-tube-rgszjyfzx-shailesh2003-archs-projects.vercel.app",
-    ],
+    origin: ["http://localhost:5173", "https://vid-tube-alpha.vercel.app"],
     credentials: true,
   })
 );
