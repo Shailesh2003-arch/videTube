c58c402a796fb37d1d83593e1794251e79f6fc4e
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-11-19 14:51:42 +0530

    add / to the options

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ app.use(
   })
 );
 
-// 🔥 absolutely needed for preflight:
-app.options("*", cors());
+// preflight fix for Express 5
+app.options("/", cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
